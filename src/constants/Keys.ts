2a6358767004f446943e765292825cbd93fb1289
lunaguima <luna.dcarvalho05@gmail.com>
2026-05-04 19:21:27 -0300

    chore: centralizando as chaves do AsyncStorage para padronizar

--- src/constants/Keys.ts
+++ src/constants/Keys.ts
@@ -1,5 +1,9 @@
 export const STORAGE_SOMA = "@App:soma";
-
 export const STORAGE_RASCUNHO = "@App:rascunho";
-
 export const STORAGE_PERFIL = "@App:perfil";
+
+
+export const STORAGE_USER_DATA = "@PetGuardian_UserData";
+export const STORAGE_LOGADO = "@PetGuardian_Logado";
+export const STORAGE_CUIDADORES = "@Matilha_Cuidadores";
+export const STORAGE_RECADOS = "@Matilha_Recados";
